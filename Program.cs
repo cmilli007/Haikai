56afe4c493e010b4019059c61a070d818beecc03
camili.costa07@gmail.com <e107ti@etec.sp.gov.br>
2023-08-14 14:55:04 -0300

    adicionar haikai

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Poesia Mínima");
+Console.Write("Pintou estrelas no muro");
+Console.Write("e teve o céu");
+Console.Write("ao alcance das maõs.");
+Console.WriteLine("");
+Console.WriteLine("Helena Kolody");
